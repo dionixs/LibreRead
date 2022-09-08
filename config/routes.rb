988215ec50f58cf7e7d3476c0f74a98b84d07954
di0nixs <di0nixs@protonmail.com>
2022-09-08 18:14:27 +0300

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+  get 'imports', to: 'import_clippings#index'
   get 'import_clippings', to: 'import_clippings#new'
   post 'import_clippings', to: 'import_clippings#create'
+  get 'import_clippings/:id(.:format)', to: 'import_clippings#show'
+  delete 'import_clippings/:id(.:format)', to: 'import_clippings#destroy'
 end
