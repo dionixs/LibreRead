f184569bdc801a0db249caccb1273a786b5e7c99
di0nixs <di0nixs@protonmail.com>
2022-09-08 21:05:58 +0300

    Add import notes from text file and save notes to db

--- app/controllers/import_clippings_controller.rb
+++ app/controllers/import_clippings_controller.rb
@@ -3,10 +3,16 @@ class ImportClippingsController < ApplicationController
     @imports = Document.all
   end
 
+  def show
+    @import  = Document.find(params[:id])
+    send_data @import.data, filename: @import.filename, disposition: 'download'
+  end
+
   def new
     @import = Document.new
   end
 
+  # todo
   def create
     @import = Document.new
     @text_file = params[:text_file]
@@ -15,14 +21,22 @@ class ImportClippingsController < ApplicationController
       @import.filename = @text_file.original_filename
       @import.mime_type = @text_file.content_type
       @import.data = File.read(@text_file.to_io)
+      @notes = ClippingsParser.new(@import.data).notes
     elsif @text_file.nil?
       flash[:notice] = 'You have not selected a file'
     else
       flash[:notice] = 'Must be a txt file'
     end
 
+    if @notes.nil?
+      flash[:notice] =  "Not a Kindle clipping file!"
+      # todo
+      render "new" and return
+    end
+
     if @import.save
-      @notes = Parser.new(@import.data).notes
+      # todo
+      @notes.each { | note | Note.new(note).save }
       redirect_to imports_path
     else
       render "new"
@@ -33,10 +47,5 @@ class ImportClippingsController < ApplicationController
     @import = Document.find(params[:id])
     @import.destroy
     redirect_to imports_path, notice:  "Successfully deleted."
-  end
-
-  def show
-    @import  = Document.find(params[:id])
-    send_data @import.data, filename: @import.filename, disposition: 'download'
   end
 end
