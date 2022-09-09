b46831ab2371807bcf00f7bf75bfa429258bed4c
di0nixs <di0nixs@protonmail.com>
2022-09-09 17:08:27 +0300

    Add header and footer

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Entry point for the build script in your package.json
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import 'flowbite';
 
 Rails.start()
 Turbolinks.start()
