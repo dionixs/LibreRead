06231ec0083dff99b5d7d4fe4c33ea32256fcd9e
di0nixs <di0nixs@protonmail.com>
2022-09-10 14:14:06 +0300

    Add validation for model Note

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,7 @@
 class Note < ApplicationRecord
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :place, presence: true
+  validates :created_kindle_at, presence: true
+  validates :clipping, presence: true
 end
