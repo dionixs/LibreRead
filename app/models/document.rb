b6561970ccf48e61192ef42c20c4d8bc7590d70d
di0nixs <di0nixs@protonmail.com>
2022-09-11 10:49:04 +0300

    Remove validate :check_file_type

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,9 +2,4 @@ class Document < ApplicationRecord
   validates :filename, presence: true, length: { in: 1..200 }
   validates :mime_type, presence: true
   validates :data, presence: true
-  validate :check_file_type
-
-  def check_file_type
-    errors.add(:document, 'Must be a Txt file') unless mime_type.in?(%w[text/plain])
-  end
 end
