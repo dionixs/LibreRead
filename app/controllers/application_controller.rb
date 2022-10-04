475e718e44a5eab74ccff23a57df97ec2405bc0c
di0nixs <di0nixs@protonmail.com>
2022-10-04 11:03:15 +0300

    Add reading and setting requested language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,26 @@ class ApplicationController < ActionController::Base
   include LanguageDetection
   include TextFileImport
   include ZipFileImport
+
+  around_action :switch_locale
+
+  private
+
+  def switch_locale(&)
+    locale = locale_from_url || I18n.default_locale
+    I18n.with_locale(locale, &)
+  end
+
+  def locale_from_url
+    locale = params[:locale]
+    locale if locale_supported?(locale)
+  end
+
+  def locale_supported?(locale)
+    I18n.available_locales.map(&:to_s).include?(locale)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module LibreRead
     config.load_defaults 7.0
 
     config.i18n.available_locales = %i[en ru]
-    config.i18n.default_locale = :ru
+    config.i18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
     #
