1c991bd71c6a06416ee65f38329eeccd00d7ba2b
di0nixs <di0nixs@protonmail.com>
2022-10-05 21:55:03 +0300

    Add locale switcher

---
+++ app/controllers/locales_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class LocalesController < ApplicationController
+  def change_locale
+    locale = params[:locale]
+    new_locale = params[:new_locale]
+    I18n.locale = locale_supported?(new_locale) ? new_locale : new_locale = locale
+    redirect_to previous_url_with_new_locale(new_locale, locale)
+  end
+
+  private
+
+  def previous_url_with_new_locale(new_locale, locale)
+    url_fragments.map { |fragment| fragment == locale ? new_locale : fragment }.join('/')
+  end
+
+  def url_fragments
+    session[:previous_url].split('/')
+  end
+end
