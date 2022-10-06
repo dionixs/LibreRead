5e52350ee57c382f7bd7dce79ac934dd0a8a6aac
di0nixs <di0nixs@protonmail.com>
2022-10-06 16:44:20 +0300

    Add gravatar

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -8,4 +8,10 @@ class UserDecorator < ApplicationDecorator
 
     email.split('@')[0]
   end
+
+  def gravatar(size: 30, css_class: '')
+    email_hash = Digest::MD5.hexdigest(email.strip.downcase)
+    h.image_tag "https://www.gravatar.com/avatar/#{email_hash}.jpg?d=retro&s=#{size}&f=y",
+                class: css_class, alt: name_or_email
+  end
 end
