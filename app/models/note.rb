ad4abaecc24490f3a1dc56548a8a37f51f90249b
di0nixs <di0nixs@protonmail.com>
2022-10-09 21:06:39 +0300

    Add new validate

--- app/models/note.rb
+++ app/models/note.rb
@@ -6,9 +6,28 @@ class Note < ApplicationRecord
   has_many :note_tags, dependent: :destroy
   has_many :tags, through: :note_tags
 
-  validates :title, presence: true
+  validates :title, presence: true, length: { minimum: 2 }
   validates :author, presence: true
   validates :place, presence: true
   validates :created_kindle_at, presence: true
   validates :clipping, presence: true
+
+  # def self.tagged_with(title)
+  #   Tag.find_by!(title:).notes
+  # end
+  #
+  # def self.tag_counts
+  #   Tag.select('tags.*, count(note_tags.tag_id) as count')
+  #      .joins(:note_tags).group('note_tags.tag_id')
+  # end
+  #
+  # def tag_list
+  #   tags.map(&:name).join(', ')
+  # end
+  #
+  # def tag_list=(titles)
+  #   self.tags = titles.split(',').map do |n|
+  #     Tag.where(name: n.strip).first_or_create!
+  #   end
+  # end
 end
