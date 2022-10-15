a28818f0a6a25fd3acbf1a64eaa58e54f2976c8d
di0nixs <di0nixs@protonmail.com>
2022-10-15 11:35:31 +0300

    Create migrate: AddIndexCreatedAtToUsers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_15_083156) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_15_083359) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_15_083156) do
     t.boolean "password_must_be_changed", default: false
     t.string "gravatar_hash"
     t.index "lower((email)::text)", name: "user_lower_email_idx", unique: true
+    t.index ["created_at"], name: "index_users_on_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
