4512cc2f6990c6f3e48892c3a5bcc6744f908b0a
di0nixs <di0nixs@protonmail.com>
2022-10-15 14:20:54 +0300

    Fix

--- app/controllers/concerns/error_handling.rb
+++ app/controllers/concerns/error_handling.rb
@@ -3,9 +3,9 @@
 module ErrorHandling
   extend ActiveSupport::Concern
 
+  # TODO
   included do
     rescue_from ActiveRecord::RecordNotFound, with: :not_found
-    rescue_from ActionController::RoutingError, with: :not_found
 
     private
 
