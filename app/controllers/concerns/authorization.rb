d431a9907300bb60412346008bfa8097804acd27
di0nixs <di0nixs@protonmail.com>
2022-10-24 12:44:21 +0300

    Refactoring concern Authorization

--- app/controllers/concerns/authorization.rb
+++ app/controllers/concerns/authorization.rb
@@ -5,6 +5,7 @@ module Authorization
 
   included do
     include Pundit::Authorization
+    include Authentication
     include Internationalization
 
     rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -13,8 +14,12 @@ module Authorization
 
     def user_not_authorized
       locale_from_session
-      flash[:danger] = t('flash.alert.not_authorized')
+      flash[:danger] = authorization_message
       redirect_to(request.referer || root_path)
+    end
+
+    def authorization_message
+      current_user.banned? ? t('flash.alert.you_are_banned') : t('flash.alert.not_authorized')
     end
   end
 end
