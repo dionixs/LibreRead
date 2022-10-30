4dd708093978d6b0fead4ba4842b426bebdca0d3
di0nixs <di0nixs@protonmail.com>
2022-10-30 15:43:04 +0300

    Create concern digestible

---
+++ app/models/concerns/digestible.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Digestible
+  extend ActiveSupport::Concern
+
+  included do
+
+    private
+
+    def digest(string)
+      cost = if ActiveModel::SecurePassword
+                .min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create(string, cost:)
+    end
+  end
+end
